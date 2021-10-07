02cb88d4e90e597797bf9602b04de32d5e892b1e
dania mustika putri <daniamustikaputri@gmail.com>
2021-10-07 11:12:28 +0700

    first commit

---
+++ HelloWord1.java
@@ -0,0 +1,8 @@
+public class HelloWord1 {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: Dania!");
+		System.out.println("NIM: 6706210088");
+		System.out.println("Kelas: 45-04");
+	}
+}
